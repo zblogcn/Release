877d88b5b0d3753e657bf3ca3eea32b0c33a9e00
zx.asd <rainbowsoft@gmail.com>
2016-06-28 00:39:47 +0800

    提交；

---
+++ zblog/index.php
@@ -0,0 +1,20 @@
+<?php
+if($_SERVER['QUERY_STRING']=='install'){
+	header("Content-type:text/xml; Charset=utf-8");
+	echo file_get_contents(__DIR__ . '/Release.xml');
+}elseif($_SERVER['QUERY_STRING']=='beta'){
+	header('Content-Type: text/plain; charset=utf-8');
+	echo file_get_contents(__DIR__ . '/beta.html');
+}elseif($_SERVER['QUERY_STRING']!=''){
+	$s=__DIR__ . '/' . str_replace('\\', '/', $_SERVER['QUERY_STRING']);
+	if(is_readable($s) && strpos($s, './')===false){
+		header('Content-Type: application/octet-stream');
+		$t=file_get_contents($s);
+		$t=str_replace("\r\n", "\n", $t);
+		$t=str_replace("\n", "\r\n", $t);
+		echo $t;
+	}
+}else{
+	header('Content-Type: text/plain; charset=utf-8');
+	echo file_get_contents(__DIR__ . '/now.html');
+}

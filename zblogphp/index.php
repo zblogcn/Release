d7008531931f9493b3884eebea1d47b850c99823
zx.asd <rainbowsoft@gmail.com>
2016-11-30 21:13:09 +0800

    提交；

--- zblogphp/index.php
+++ zblogphp/index.php
@@ -10,6 +10,18 @@ if($_SERVER['QUERY_STRING']=='install'){
 	if(is_readable($s) && strpos($s,'./')===false){
 		header('Content-Type: application/octet-stream');
 		echo file_get_contents($s);
+	}elseif(is_readable($s)==false){
+		$regex_otherver= '/[0-9]{6}-[0-9]{6}\.xml/i';
+		$xml = __DIR__ . '/' . 'builds.xml';
+		$xml = file_get_contents($xml);
+	    $xml = @simplexml_load_string($xml);
+	    reset($xml->build);
+	    $v1='150101';//current($xml->build);
+	    $v2=end($xml->build);
+        if(preg_match( $regex_otherver, str_replace('\\','/',$_SERVER['QUERY_STRING']) ) != 0 ){
+			header('Content-Type: application/octet-stream');
+			echo file_get_contents(__DIR__ . '/' . $v1 . '-' . $v2 . '.xml');
+        }
 	}
 }else{
 	header('Content-Type: text/plain; charset=utf-8');

2581944ff19798d70564dd0a09d1318574595a62
zx.asd <rainbowsoft@users.noreply.github.com>
2020-06-02 09:54:44 +0800

    162120

---
+++ info/index.php
@@ -0,0 +1,37 @@
+<!-- style="color:crimson"  style="color:#ff6600" style="color:blue"-->
+
+<p><a href="https://blog.zblogcn.com/2020/06/01/110/" target="_blank"><b>滴滴滴，发车了！Z-BlogPHP 1.6.4 Valyria发布了! (2020-6-1)</b></a></p>
+
+<p>[Wiki]&nbsp;&nbsp;
+<a href="https://wiki.zblogcn.com/doku.php?id=zblogphp:changelog" target="_blank">ZBlogPHP详细更新日志</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="https://wiki.zblogcn.com/doku.php?id=zblogphp:development" target="_blank">ZBlogPHP应用开发指南</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="https://wiki.zblogcn.com/doku.php?id=zblogphp:development:migration:1.5_-_1.6" target="_blank">开发者更新指南</a>
+</p>
+
+<p>[故障处理]&nbsp;&nbsp;
+<a href="https://wiki.zblogcn.com/doku.php?id=zblogphp:failure:forgetpassword" target="_blank">ZBlogPHP密码找回工具</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="https://wiki.zblogcn.com/doku.php?id=zblogphp:failure:permanentdomainerror" target="_blank">固定域名出错</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="https://wiki.zblogcn.com/doku.php?id=zblogphp:failure:cannotemoji" target="_blank">不能发Emoji表情</a>
+</p>
+
+<!--<p><a href="https://blog.zblogcn.com/2018/03/29/98/" target="_blank"><b>2018年3月29日应用中心安全更新公告(2018-3-29)</b></a></p>
+-->
+<p><a href="https://blog.zblogcn.com/2018/02/12/99/" target="_blank">大吉大利，天天吃鸡——Z-Blog祝您2018新春快乐，应用中心红包活动开始了！ (2018-2-12)</a></p>
+
+<!-- <p><a style="color:crimson"  href="https://app.zblogcn.com/11.php" target="_blank">Z-Blog应用中心“双十一”大促活动 (2017-11-09)</a></p>-->
+
+<!-- <p><a href="https://blog.zblogcn.com/2017/03/28/90/" target="_blank">ZBLOG官方关于多说停止运营的公告！ (2017-3-28)</a></p>-->
+
+<!-- <p><a href="https://blog.zblogcn.com/2017/01/23/87/" target="_blank">祝大家鸡年大吉——Z-BlogPHP 1.5.1 发布了！ (2017-1-23)</a></p>-->
+
+<!--<p><a href="https://blog.zblogcn.com/2016/11/27/86/" target="_blank">当当当当当当当，Z-Blog系列服务全站升级了！(2016-11-28)</a></p>
+-->
+<!-- <p><a href="https://www.zblogcn.com/" target="_blank">Z-Blog启用新域名：www.zblogcn.com，原域名rainbowsoft.org已不再使用。(2014-2-21)</a></p>-->
+
+<p><a href="https://bbs.zblogcn.com/thread-83785-1-1.html" target="_blank">2014年ASP版全新发布！Z-Blog 2.2 Prism Build 140101 发布了。(2014-01-02)</a></p>
+
+<p><a href="https://bbs.zblogcn.com/thread-77001-1-1.html" target="_blank">Z-Blog应用中心正式上线，欢迎开发者进驻。(2013-01-01)</a></p>
